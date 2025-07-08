13b4867c0472a61e873a3baf47fc6f8b4fb02dd1
Hritesh Roshan <127621507+Hritesh49@users.noreply.github.com>
2025-07-08 14:49:47 +0530

    port added

--- server/server.js
+++ server/server.js
@@ -29,10 +29,12 @@ app.use("/api/task", taskRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 // ✅ MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("✅ MongoDB Connected");
-        app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`));
+        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
     })
     .catch(err => console.error("❌ MongoDB Connection Error:", err));
